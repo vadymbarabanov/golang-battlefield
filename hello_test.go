3db5ebfda3465e4038238439ba58b18868b5ebb0
Vadym Barabanov <introvert.png@gmail.com>
2021-10-06 21:42:35 +0300

    hello TDD example

---
+++ hello_test.go
@@ -0,0 +1,23 @@
+package main
+
+import "testing"
+
+func TestHello(t *testing.T) {
+	asserCorrectMessage := func(t testing.TB, got, want string) {
+		t.Helper()
+		if got != want {
+			t.Errorf("got %q want %q", got, want)
+		}
+	}
+
+	t.Run("saying hello to people", func(t *testing.T) {
+		got := Hello("Robin")
+		want := "Hello, Robin"
+		asserCorrectMessage(t, got, want)
+	})
+	t.Run("say 'Hello, world' when an empty string is supplied", func(t *testing.T) {
+		got := Hello("")
+		want := "Hello, world"
+		asserCorrectMessage(t, got, want)
+	})
+}
